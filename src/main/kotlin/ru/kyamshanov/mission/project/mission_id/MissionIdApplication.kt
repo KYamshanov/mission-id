2865260792ed526c5007e9892ef6ac98878125b9
Ямшанов Константин Алексеевич (4059153) <yamshanov@dev.vtb.ru>
2023-03-08 13:00:30 +0400

    Initial commit

---
+++ src/main/kotlin/ru/kyamshanov/mission/project/mission_id/MissionIdApplication.kt
@@ -0,0 +1,11 @@
+package ru.kyamshanov.mission.project.mission_id
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MissionIdApplication
+
+fun main(args: Array<String>) {
+    runApplication<MissionIdApplication>(*args)
+}
